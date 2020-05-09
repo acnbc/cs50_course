aac9b366d3d60adedd9b5695a55926a78cac2b04
Ana Carolina Cabral <62575040+acnbc@users.noreply.github.com>
2020-05-09 19:02:50 -0300

    Add files via upload
    Problem Set Week 1 - Hello

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void hello(string name);
+
+int main(void)
+{
+    
+    hello("Ana");
+    
+    
+    
+    
+    void hello(string name)
+    {
+        printf("Hello, %s\n", name);
+    }
+}
